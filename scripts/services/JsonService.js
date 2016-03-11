0b8d25fd714d56145237c6620a724a7294e16502
alberto barquin <alberto@barquin.es>
2016-03-11 14:45:28 +0100

    jsonService modified to support brands.json

--- scripts/services/JsonService.js
+++ scripts/services/JsonService.js
@@ -2,15 +2,21 @@
  * Created by albertobarquin on 2/12/15.
  */
 angular.module("cemasa").service("JsonService", [ '$http', function($http) {
-    var jsonUrl = "data/data.json";
+    var productsUrl = "data/data.json";
+    var brandsUrl = "data/brands.json";
 
     delete $http.defaults.headers.common['X-Requested-With'];
 
     this.getData = function(){
-        return $http.get(jsonUrl, {
+        return $http.get(productsUrl, {
            cache: true
        });
 
+    }
+    this.getBrands = function(){
+        return $http.get(jsonUrl, {
+            cache: true
+        });
 
     }
 
